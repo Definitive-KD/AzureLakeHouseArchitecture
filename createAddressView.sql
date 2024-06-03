821be8059710ff44a7f5aca456f5adcd2e6f8802
Definitive-KD <54216704+Definitive-KD@users.noreply.github.com>
2024-06-03 17:17:13 -0400

    Delete createAddressView.sql

--- createAddressView.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW address
-AS
-SELECT  
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://migrationstorageks.dfs.core.windows.net/gold/SalesLT/Address/',
-        FORMAT = 'DELTA'
-    ) AS [result]
